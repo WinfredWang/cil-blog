ffe9f1111926fa2452a743b0108e7d31d49f19d1
Winfred <wangqiyong@126.com>
2018-05-30 21:25:27 +0800

    增加logo

--- src/client/components/Navbar.vue
+++ src/client/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="navbar-area">
       <el-menu mode="horizontal" :default-active="$route.path" :router="true">
+        <div class="logo">
+          一箭中的的日志
+        </div>
         <el-menu-item v-for="item in items" v-bind:key="item.url" :index="item.url">
           {{item.name}}
         </el-menu-item>
@@ -16,6 +19,17 @@ export default {
 <style>
 #navbar-area .el-menu {
   padding: 0% 10%;
+}
+.logo {
+  float: left;
+  height: 60px;
+  line-height: 60px;
+  font-size: 20pt;
+  color: #ff9d00;
+  cursor: pointer;
+}
+.logo:hover {
+  opacity: 0.5;
 }
 #navbar-area .el-menu--horizontal .el-menu-item {
   float: right;
